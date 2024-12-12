5d10fede485c6a327d4ed5f74e8c0a6b2013cf35
gmltjd8002@naver.com <gmltjd8005@gmail.com>
2024-12-12 15:16:55 +0900

    project initialized in 2024-12-12

---
+++ src/test/java/com/workout/workoutManager/WorkoutManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workout.workoutManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
